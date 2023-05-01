9bee3e8597a9b599db047ae62f3dfc44d7529e67
Jeff Oliver <jeffoliver3384@gmail.com>
2023-05-01 02:51:55 -0400

    Fixed error in firebase config

--- firebase.ts
+++ firebase.ts
@@ -1,16 +1,12 @@
 import admin, { ServiceAccount } from "firebase-admin";
-import serviceAccount from "@/permissions.json";
 
-const PRODUCTION_ENV: boolean = process.env.NODE_ENV !== "test";
 const FIREBASE_CONFIG: admin.ServiceAccount = process.env.FIREBASE_CONFIG as ServiceAccount;
 
 type App = admin.app.App;
 
 const app: App = !admin.apps.length
 	? admin.initializeApp({
-			credential: admin.credential.cert(
-				PRODUCTION_ENV ? FIREBASE_CONFIG : (serviceAccount as ServiceAccount)
-			),
+			credential: admin.credential.cert(FIREBASE_CONFIG),
 	  })
 	: admin.app();
 
